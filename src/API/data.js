8a769180f8ecbf913617868f614e281e2e65ae40
“tlvu93” <101603072+tlvu93@users.noreply.github.com>
2022-04-28 17:42:54 +0200

    Add Contentful API

---
+++ src/API/data.js
@@ -0,0 +1,66 @@
+import credentials from './credentials.json';
+const contentful = require('contentful');
+
+const client = contentful.createClient(credentials);
+
+const getAllData = async () => {
+  const data = await client.getEntries({
+    // EVERYTHING!
+  });
+
+  console.log('All', data);
+  return data.items;
+};
+
+const getRecipes = async () => {
+  const data = await client.getEntries({
+    content_type: 'recipeGroup3',
+  });
+  return data.items;
+};
+
+// const getBlogs = async () => {
+//     const data = await client.getEntries({
+//         content_type: 'blog',
+//     });
+//     console.log('Blogs', data);
+//     return data.items
+// };
+
+// const getAuthors = async () => {
+//     const data = await client.getEntries({
+//         content_type: 'author',
+//         select: 'sys.createdAt,fields.bio,fields.name,fields.description',
+//     });
+//     console.log('Authors', data);
+//     return data.items
+// };
+
+// const getComments = async () => {
+//     const data = await client.getEntries({
+//         content_type: 'comment'
+//     });
+//     console.log('Comments', data);
+//     return data.items
+
+// }
+
+// const getCommentsByArticle = async (id) => {
+//     const data = await client.getEntries({
+//         content_type: 'comment',
+//         'fields.replyTo.sys.id': id
+//     });
+//     console.log('Comments by article ' + id, data);
+//     return data.items
+// }
+
+export {
+  getAllData,
+  getRecipes,
+  // getAuthors,
+  // getComments,
+  // getCommentsByArticle,
+  // getBlogs
+};
+
+export default client;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import MainPage from "./components/mainpage";
-import Recipe from "./components/recipe";
+import MainPage from './components/mainpage';
+import Recipe from './components/recipe';
+import { useEffect, useState } from 'react';
+
 import './App.css';
+import { getRecipes, getAllData } from './API/data';
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setRecipes(await getRecipes());
+    })();
+  }, []);
+
   return (
     <div>
-     <MainPage />
-     <Recipe />
+      {recipes.map((recipe) => {
+        return (
+          <div>
+            <p>{recipe.fields.name}</p>
+            <p>{recipe.fields.description}</p>
+          </div>
+        );
+      })}
+      <MainPage />
+      <Recipe />
     </div>
   );
 }
